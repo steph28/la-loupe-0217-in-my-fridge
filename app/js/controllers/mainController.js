10bf441c54b7386b83f2ecd36f2701f7faf43e25
madasly <madasly@gmail.com>
2017-03-30 14:29:09 +0200

    un peu plus responsive

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,7 +1,8 @@
 angular.module('app')
     .controller('MainController', function($scope, recipeService) {
       recipeService.getAll().then(function(res){
-        $scope.all = res.data ;
+        $scope.all = 'res.data' ;
+        $scope.searchRecipes = '';
         console.log(res.data);
       });
       /* Here is your main controller */
