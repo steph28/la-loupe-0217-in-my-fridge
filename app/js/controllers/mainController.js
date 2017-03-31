a2cf7aafb4028034e14736fc6cddb6f851a3d079
Michel Domingos <m.domingos76@gmail.com>
2017-03-31 15:17:11 +0200

    add choice category

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,14 +1,23 @@
 angular.module('app')
     .controller('MainController', function($scope, recipeService) {
-      $scope.searchRecipes = '';
-      $scope.listIngredients = '';
-      $scope.category = '',
+
+        $scope.meat = ['Bacon', 'Beef', 'Breast', 'Bushmeat', 'Chicken', 'Chorizo', 'Duck', 'Goose', 'Ground', 'Ham', 'Mutton', 'Pepperoni', 'Pork', 'Poultry', 'Quail', 'Rabbit', 'Sausage cooked', 'Turkey', 'Veal', 'Venison', 'Wild boar'];
+        $scope.dairy = ['Cheddar', 'Mozzarella', 'Breast', 'Bushmeat', 'Chicken', 'Chorizo', 'Duck', 'Goose', 'Ground', 'Ham', 'Mutton', 'Pepperoni', 'Pork', 'Poultry', 'Quail', 'Rabbit', 'Sausage cooked', 'Turkey', 'Veal', 'Venison', 'Wild boar'];
+        $scope.fish = ['Fish'];
+        $scope.fruits = ['Fruits'];
+        $scope.spices = ['Spices'];
+        $scope.vegetables = ['Vegetables'];
+
+        $scope.ingredients = $scope.dairy;
 
 
 
-      recipeService.getAll().then(function(res){
-        $scope.all = 'res.data' ;
-        console.log(res.data);
-      });
-      /* Here is your main controller */
+
+
+
+        recipeService.getAll().then(function(res) {
+            $scope.all = 'res.data';
+            console.log(res.data);
+        });
+        /* Here is your main controller */
     });
