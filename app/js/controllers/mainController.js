c2c51809070bf02d959d89caa2f359957da94d90
madasly <madasly@gmail.com>
2017-04-04 16:03:57 +0200

    dernier test api avant css

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,76 +1,67 @@
 angular.module('app')
-    .controller('MainController', function($scope, recipeService) {
+    .controller('MainController', function($scope, $location, recipeService) {
 
-      $scope.errors = [];
+        $scope.errors = [];
 
-      $scope.login = function() {
-          if ($scope.loginForm.$valid) {
-              $scope.errors = [];
-              Auth.login($scope.user).then(function(result) {
-                  $state.go('user.profile');
-              }).catch(function(err) {
-                  $scope.errors.push(err);
-              });
-          }
+        $scope.login = function() {
+            if ($scope.loginForm.$valid) {
+                $scope.errors = [];
+                Auth.login($scope.user).then(function(result) {
+                    $state.go('user.profile');
+                }).catch(function(err) {
+                    $scope.errors.push(err);
+                });
+            }
+        };
+
+        $scope.register = function() {
+            Auth.register($scope.user).then(function() {
+                $state.go('anon.home');
+            });
+        };
+        $scope.addSelected = "";
+        $scope.selected = '';
+        // faire table pour mettre chq categ dans tableau et ingredients
+
+
+
+        $scope.category = function(nb){
+          var table = [$scope.dairy,$scope.meat, $scope.fish,$scope.fruits, $scope.spices, $scope.vegetables];
+
+        $scope.dairy = ['Cheddar', 'Mozzarella', 'Breast', 'Bushmeat', 'Chicken', 'Chorizo', 'Duck', 'Goose', 'Ground', 'Ham', 'Mutton', 'Pepperoni', 'Pork', 'Poultry', 'Quail', 'Rabbit', 'Sausage cooked', 'Turkey', 'Veal', 'Venison', 'Wild boar'];
+        $scope.meat = ['Bacon', 'Beef', 'Breast', 'Bushmeat', 'Chicken', 'Chorizo', 'Duck', 'Goose', 'Ground', 'Ham', 'Mutton', 'Pepperoni', 'Pork', 'Poultry', 'Quail', 'Rabbit', 'Sausage cooked', 'Turkey', 'Veal', 'Venison', 'Wild boar'];
+        $scope.fish = ['salmon'];
+        $scope.fruits = [ 'orange', 'banana', 'strawberry'];
+        $scope.spices = [ 'pepper'];
+        $scope.vegetables =['potatoes'];
+console.log(table);
+        $scope.ingredients = table[nb];
       };
 
-      $scope.register = function() {
-          Auth.register($scope.user).then(function() {
-              $state.go('anon.home');
-          });
-      };
 
-$scope.selected = '';
-        $scope.meat = [{meat:'Bacon'},{meat:'Beef'} , {meat:'Breast'}, {meat:'Bushmeat'},{meat:'Chicken' } ,{meat:'Chorizo' } ,{meat:'Duck'} , {meat:'Goose'},{meat:'Ground'},{meat:'Ham'},{meat:'Mutton'}, {meat:'Pepperoni'},{meat:'Pork'},{meat:'Poultry'},{meat:'Quail'},{meat:'Rabbit'},{meat:'Sausage cooked'},{meat:'Turkey'},{meat:'Veal'},{meat:'Venison'},{meat:'Wild boar'}];
-        $scope.dairy = [{dairy:'Cheddar'},{dairy:'Mozzarella'}, 'Breast', 'Bushmeat', 'Chicken', 'Chorizo', 'Duck', 'Goose', 'Ground', 'Ham', 'Mutton', 'Pepperoni', 'Pork', 'Poultry', 'Quail', 'Rabbit', 'Sausage cooked', 'Turkey', 'Veal', 'Venison', 'Wild boar'];
-        $scope.fish = [{fish:'salmon'}];
-        $scope.fruits = [{fruits:'orange'},{fruits:'banana'},{fruits:'strawberry'}];
-        $scope.spices = [{spices:'pepper'}];
-        $scope.vegetables = ['Vegetables'];
-
-        $scope.ingredients = $scope.meat;
+        var selected = '';
+        $scope.recipes = [];
+        $scope.showRecipe1 = function() {
+            recipeService.getSearch($scope.selected).then(function(res) {
 
 
-var selected = '';
-  $scope.recipes = [];
-        $scope.showRecipe1 = function () {
-          recipeService.getSearch($scope.selected).then(function(res) {
-            var i = 0;
-            // for (var i = 0; i < 11; i++);
-            $scope.image = (res.data.recipes[i].image_url);
-            $scope.title = (res.data.recipes[i].title);
-            $scope.site = (res.data.recipes[i].publisher_url);
-            $scope.source = (res.data.recipes[i].source_url);
-            console.log(res.data.recipes);
+                console.log(res.data.recipes);
+                $location.path("/test");
 
-          });
+            });
 
 
         };
-        $scope.showRecipe1();
+        
 
-        $scope.showRecipe = function () {
-          var i = 0;
+        $scope.showRecipe = function() {
+          recipeService.getAll($scope.selected).then(function(res){
 
-        recipeService.getAll($scope.selected).then(function(res) {
+                console.log(res.data.recipes);
+                $scope.datas = res.data.recipes;
 
-          $scope.image = (res.data.recipes[i].image_url);
-          $scope.title = (res.data.recipes[i].title);
-          $scope.site = (res.data.recipes[i].publisher_url);
-          $scope.source = (res.data.recipes[i].source_url);
-          console.log(res.data.recipes);
-          $scope.recipes = res.data.recipes;
-        });
+            });
 
-      };
-      $scope.showRecipe();
+        };
+        $scope.showRecipe();
     });
-
-
-
-        // recipeService.getAll().then(function(response){
-        // console.log('mes recettes récuperées');
-        // }, function (error) {
-        //   console.log(' il n y a pas de recettes');
-        //
-        // });
--- app/js/services/recipeService.js
+++ app/js/services/recipeService.js
@@ -3,11 +3,13 @@ angular.module('app')
 
         return {
           getSearch: function(x) {
-            return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=12&q=' + x);
+            return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=4&q=' + x);
             },
             getAll: function(x) {
-              return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=12&q=' + x);
+              return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=4&q=' + x);
             },
           };
         });
 // <!-- faire regex pour virgules ou espaces  -->
+// var table = [$scope.dairy,$scope.meat, $scope.fish,$scope.fruits, $scope.spices, $scope.vegetables];
+// console.log(table);
