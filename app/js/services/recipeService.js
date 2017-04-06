695b09f08428ea4e59fb1af96e2c94589acdc5b7
madasly <madasly@gmail.com>
2017-04-06 21:44:38 +0200

    le retour gagnant

--- app/js/services/recipeService.js
+++ app/js/services/recipeService.js
@@ -3,13 +3,14 @@ angular.module('app')
 
         return {
           getSearch: function(x) {
-            return $http.get('http://food2fork.com/api/search?key=9851f031a895322d4b1b9858d4fdee75&count=4&q=' + x);
+            return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=4&q=' + x);
             },
             getAll: function(x) {
-              return $http.get('http://food2fork.com/api/search?key=9851f031a895322d4b1b9858d4fdee75&count=4&q=' + x);
+              return $http.get('http://food2fork.com/api/search?key=a9d96f05e69633450b746a68935e7b83&count=4&q=' + x);
             },
             getById: function(id) {
-              return $http.get('http://food2fork.com/api/get?key=9851f031a895322d4b1b9858d4fdee75&rId=' + id);
+              return $http.get('http://food2fork.com/api/get?key=a9d96f05e69633450b746a68935e7b83&rId=' + id);
             },
           };
           });
+// key=9851f031a895322d4b1b9858d4fdee75
