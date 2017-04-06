a9fb369b604c5a8004e6edfa893b7c129d8c72e5
madasly <madasly@gmail.com>
2017-04-06 12:17:24 +0200

    bonne  page id

--- app/js/controllers/detailController.js
+++ app/js/controllers/detailController.js
@@ -1,9 +1,11 @@
 angular.module('app')
     .controller('DetailController', function($scope, $stateParams, recipeService) {
-
+      $scope.recipe = "";
 
         recipeService.getById($stateParams.id).then(function(res) {
           console.log(res.data);
+          $scope.recipe = res.data.recipe;
+          $scope.ingredients = res.data.recipe.ingredients;
         });
 
     });
